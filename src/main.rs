7170cd1e3bf4b055696c16ebc2bcadf863677c86
brookman <rc-p90@bluemail.ch>
2024-02-28 07:59:34 +0100

    Add WiP Rust version of bigram.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,58 @@
+use anyhow::{Context, Result};
+use candle_core::{Device, NdArray, Tensor};
 use itertools::Itertools;
-use std::{
-    collections::{HashMap, HashSet},
-    fs,
-};
+use std::{collections::HashMap, fs};
 
 struct Encoder<'a> {
-    i_to_c:HashMap<u8,&'a char>,
-    c_to_i:HashMap<&'a char, u8>,
+    i_to_c: HashMap<u8, &'a char>,
+    c_to_i: HashMap<&'a char, u8>,
 }
 
-impl<'a>  Encoder<'a> {
-    
-}
-
-fn main() {
-    let text = fs::read_to_string("data/input.txt").unwrap();
-    let vocab = text.chars().unique().sorted().collect::<Vec<_>>();
-    let i_to_c = vocab
-        .iter()
-        .enumerate()
-        .map(|(i, c)| (i as u8, c))
-        .collect::<HashMap<_, _>>();
-    let c_to_i = vocab
-        .iter()
-        .enumerate()
-        .map(|(i, c)| (c, i as u8))
-        .collect::<HashMap<_, _>>();
-
-    fn encode(s: String) -> Vec<u8> {
-        s.chars().map(|c|c_to_i[])
+impl<'a> Encoder<'a> {
+    fn new(vocab: &'a [char]) -> Self {
+        let i_to_c = vocab
+            .iter()
+            .enumerate()
+            .map(|(i, c)| (i as u8, c))
+            .collect::<HashMap<_, _>>();
+        let c_to_i = vocab
+            .iter()
+            .enumerate()
+            .map(|(i, c)| (c, i as u8))
+            .collect::<HashMap<_, _>>();
+        Self { i_to_c, c_to_i }
     }
-    println!("{:?}", vocab);
+
+    fn encode(&self, s: String) -> Result<Vec<u8>> {
+        s.chars()
+            .map(|c| self.c_to_i.get(&c).copied().context("Can't map"))
+            .into_iter()
+            .collect()
+    }
+
+    fn decode(&self, vec: Vec<u8>) -> Result<String> {
+        let x = vec
+            .iter()
+            .map(|i| self.i_to_c.get(i).context("Can't map"))
+            .collect::<Result<Vec<_>>>()?;
+        Ok(x.iter().map(|c| *c).join(""))
+    }
+}
+
+fn main() -> Result<()> {
+    let device = Device::Cpu;
+
+    let text = fs::read_to_string("data/input.txt")?;
+    let vocab = text.chars().unique().sorted().collect::<Vec<_>>();
+    let encoder = Encoder::new(&vocab);
+
+    let encoded_text = encoder.encode(text)?;
+    let len = encoded_text.len();
+
+    let data = Tensor::from_vec(encoded_text, len, &device)?;
+    println!("data shape {:?}", data.shape());
+
+    let n = (0.9 * len as f64) as usize;
+
+    Ok(())
 }
